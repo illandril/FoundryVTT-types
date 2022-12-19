a182515f6b22f889ae778c7e295d0c924cf45f5c
Joe Spandrusyszyn <github@illandril.net>
2022-12-19 00:40:38 -0500

    Fix indentation

--- src/foundry/utils/Collection.d.ts
+++ src/foundry/utils/Collection.d.ts
@@ -13,39 +13,37 @@ declare global {
         get contents(): V[];
 
         /**
-       * Find an entry in the Map using a functional condition.
-       */
+         * Find an entry in the Map using a functional condition.
+         */
         find(condition: CollectionFilter<K, V, Collection<K, V>>): V | undefined;
 
         /**
-       * Filter the Collection, returning an Array of entries which match a functional condition.
-       */
+         * Filter the Collection, returning an Array of entries which match a functional condition.
+         */
         filter(condition: CollectionFilter<K, V, Collection<K, V>>): V[];
 
-        /* -------------------------------------------- */
-
         /**
-       * Apply a function to each element of the collection
-       */
+         * Apply a function to each element of the collection
+         */
         forEach(fn: (value: V) => void): void;
 
         /**
-       * Get an element from the Collection by its key.
-       * Returns undefined if it does not exist
-       */
+         * Get an element from the Collection by its key.
+         * Returns undefined if it does not exist
+         */
         get(key: K): V | undefined;
 
         /**
-       * Get an element from the Collection by its key.
-       * Throws if it does not exist
-       */
+         * Get an element from the Collection by its key.
+         * Throws if it does not exist
+         */
         get(key: K, options: { strict: true }): V;
 
         /**
-       * Get an entry from the Collection by name.
-       * Use of this method assumes that the objects stored in the collection have a "name" attribute.
-       * Returns undefined if it does not exist
-       */
+         * Get an entry from the Collection by name.
+         * Use of this method assumes that the objects stored in the collection have a "name" attribute.
+         * Returns undefined if it does not exist
+         */
         getName(name: string): V | undefined;
 
         /**
@@ -56,21 +54,21 @@ declare global {
         getName(name: string, options: { strict: true }): V;
 
         /**
-       * Transform each element of the Collection into a new form, returning an Array of transformed values
-       * @param {function(*,number,Collection): *} transformer  A transformation function applied to each entry value.
-       * Positional arguments are the value, the index of iteration, and the collection being mapped.
-       * @return {Array<*>}  An Array of transformed values
-       */
+         * Transform each element of the Collection into a new form, returning an Array of transformed values
+         * @param {function(*,number,Collection): *} transformer  A transformation function applied to each entry value.
+         * Positional arguments are the value, the index of iteration, and the collection being mapped.
+         * @return {Array<*>}  An Array of transformed values
+         */
         map<T>(transformer: (value: V, index: number, collection: Collection<K, V>) => T): T[];
 
         /**
-       * Reduce the Collection by applying an evaluator function and accumulating entries
-       */
+         * Reduce the Collection by applying an evaluator function and accumulating entries
+         */
         reduce<T>(reducer: (acc: T, value: V, index: number, collection: Collection<K, V>) => T, initial: T): T;
 
         /**
-       * Test whether a condition is met by some entry in the Collection.
-       */
+         * Test whether a condition is met by some entry in the Collection.
+         */
         some(condition: CollectionFilter<K, V, Collection<K, V>>): boolean;
       }
     }
