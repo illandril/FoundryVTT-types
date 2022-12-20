8b04effae17affc6ce715b5eb229a3966c4d8dce
Joe Spandrusyszyn <github@illandril.net>
2022-12-20 18:39:13 -0500

    Add actorLink

--- src/client/documents/TokenDocument.d.ts
+++ src/client/documents/TokenDocument.d.ts
@@ -3,6 +3,7 @@ import { TokenDocumentConstructor } from './constructors';
 declare global {
   class TokenDocument extends TokenDocumentConstructor {
     get actor(): Actor;
+    get actorLink(): boolean;
   }
 }
 
