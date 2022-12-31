0ec0eb39c899a47590047f2330b23a51315c1d38
Joe Spandrusyszyn <git@illandril.net>
2022-12-31 18:25:31 -0500

    Fix actor embedded collection types

--- src/client/documents/Actor.d.ts
+++ src/client/documents/Actor.d.ts
@@ -6,9 +6,9 @@ declare global {
     ItemType extends Item = Item,
     ActiveEffectType extends ActiveEffect = ActiveEffect,
   > extends ActorConstructor {
-    get effects(): ActiveEffectType[];
+    get effects(): EmbeddedCollection<string, ActiveEffectType>;
     get temporaryEffects(): ActiveEffectType[];
-    get items(): ItemType[];
+    get items(): EmbeddedCollection<string, ItemType>;
   }
 }
 
--- src/foundry/documents/BaseActor.d.ts
+++ src/foundry/documents/BaseActor.d.ts
@@ -5,8 +5,8 @@ declare global {
         static TYPES: string[];
         get name(): string;
         get type(): typeof foundry.documents.BaseActor.TYPES[number];
-        get effects(): BaseActiveEffect[];
-        get items(): BaseItem[];
+        get effects(): EmbeddedCollection<string, BaseActiveEffect>;
+        get items(): EmbeddedCollection<string, BaseItem>;
       }
     }
   }
