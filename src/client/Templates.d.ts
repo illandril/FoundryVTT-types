ae115b829b6a5b2e751c488d935e13cfb6d6922c
Joe Spandrusyszyn <git@illandril.net>
2023-01-08 17:29:59 -0500

    Expose some global values on the Window interface

--- src/client/Templates.d.ts
+++ src/client/Templates.d.ts
@@ -3,6 +3,11 @@ import { TemplateDelegate } from 'handlebars';
 declare global {
   const getTemplate: <T extends object>(path: string, id?: string) => Promise<TemplateDelegate<T>>;
   const renderTemplate: <T extends object>(path: string, data: T) => Promise<string>;
+
+  interface Window {
+    getTemplate: typeof getTemplate
+    renderTemplate: typeof renderTemplate
+  }
 }
 
 export {};
--- src/client/variables.d.ts
+++ src/client/variables.d.ts
@@ -2,6 +2,12 @@ declare global {
   const vtt: string;
   const game: Game;
   const jQuery: JQuery;
+
+  interface Window {
+    vtt: typeof vtt
+    game: typeof game
+    jQuery: typeof jQuery
+  }
 }
 
 export {};
