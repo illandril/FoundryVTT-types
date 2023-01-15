b5e3a72861e595ceaa9885e80dde509484d5b716
Joe Spandrusyszyn <git@illandril.net>
2023-01-15 18:32:33 -0500

    Expand BaseToken type

--- src/foundry/documents/BaseToken.d.ts
+++ src/foundry/documents/BaseToken.d.ts
@@ -3,10 +3,21 @@ declare global {
     namespace documents {
       class BaseToken extends foundry.abstract.Document {
         get actorId(): string;
+        get actorLink(): boolean;
+
         get texture(): foundry.data.TextureData;
-        get effects(): string[];
+
         get width(): number;
         get height(): number;
+        get x(): number;
+        get y(): number;
+
+        get effects(): string[];
+        get overlayEffect(): string;
+
+        get hidden(): boolean;
+
+        get disposition(): number;
       }
     }
   }
