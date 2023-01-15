c57c7b63d916fcd1912f93fe98977da6dab9114f
Joe Spandrusyszyn <git@illandril.net>
2023-01-15 18:37:22 -0500

    Expand Token type

--- src/client/canvas/Token.d.ts
+++ src/client/canvas/Token.d.ts
@@ -1,8 +1,14 @@
 declare global {
   class Token extends PlaceableObject<TokenDocument> {
     get actor(): Actor;
+
+    get owner(): boolean;
+    get observer(): boolean;
+
     get isVisible(): boolean;
+
     get effects(): PIXI.Container;
+
     drawEffects(): Promise<void>;
     _refreshEffects(): void;
   }
