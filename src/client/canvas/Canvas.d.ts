39006f0aa5c06f9e96c0baf9c0a2d8beae268c76
Joe Spandrusyszyn <git@illandril.net>
2023-01-23 15:37:03 -0500

    Add grid types

--- src/client/canvas/Canvas.d.ts
+++ src/client/canvas/Canvas.d.ts
@@ -1,10 +1,13 @@
 declare global {
   class Canvas {
     get ready(): boolean;
-    get tokens(): TokenLayer | undefined;
-    get hud(): HeadsUpDisplay | undefined;
-    get dimensions(): SceneDimensions | undefined;
+
     get stage(): PIXI.Container | undefined;
+    get grid(): GridLayer | null;
+    get hud(): HeadsUpDisplay | undefined;
+    get tokens(): TokenLayer | undefined;
+
+    get dimensions(): SceneDimensions | undefined;
     get scene(): Scene | null;
 
     animatePan(args: Partial<CanvasViewPosition> & { duration?: number, speed?: number }): Promise<unknown>;
