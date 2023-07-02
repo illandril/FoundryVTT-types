72f39765385391517d33d354742b68568ff3d0a6
Joe Spandrusyszyn <git@illandril.net>
2023-07-02 18:19:19 -0400

    Fix MouseInteractionManager.callback type def

--- src/client/MouseInteractionManager.d.ts
+++ src/client/MouseInteractionManager.d.ts
@@ -1,28 +1,20 @@
 
-interface CallbackTypes {
-  hoverIn: never
-  hoverOut: never
-  clickLeft: never
-  clickLeft2: never
+interface CallbackWithArgsTypes {
   longPress: [PIXI.Point]
-  clickRight: never
-  clickRight2: never
-  dragLeftStart: never
-  dragRightStart: never
-  dragLeftMove: never
-  dragRightMove: never
-  dragLeftDrop: never
-  dragRightDrop: never
-  dragLeftCancel: never
-  dragRightCancel: never
 }
 
+type CallbackWithoutArgsTypes =
+  `hover${'In' | 'Out'}`
+  | `click${'Left' | 'Right'}${'' | '2'}`
+  | `drag${'Left' | 'Right'}${'Start' | 'Move' | 'Drop' | 'Cancel'}`;
+
+type CallbackTypes = CallbackWithoutArgsTypes | keyof CallbackWithArgsTypes;
 declare global {
   class MouseInteractionManager {
-    can(action: keyof CallbackTypes, event: Event): boolean;
-    callback<T extends keyof CallbackTypes>(action: T, event: Event, ...args: CallbackTypes[T]): void;
+    can(action: CallbackTypes, event: Event): boolean;
+    callback<T extends keyof CallbackWithArgsTypes>(action: T, event: Event, ...args: CallbackWithArgsTypes[T]): void;
+    callback<T extends CallbackWithoutArgsTypes>(action: T, event: Event): void;
   }
 }
 
 export {};
-
