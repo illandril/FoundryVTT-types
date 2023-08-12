b4f1a7ad989d30736c5d4b339d4fb35a7140cbe0
Joe Spandrusyszyn <git@illandril.net>
2023-08-12 14:56:03 -0400

    Add elevation and rotation token fields

--- src/foundry/documents/BaseToken.d.ts
+++ src/foundry/documents/BaseToken.d.ts
@@ -13,6 +13,8 @@ declare global {
         height: number
         x: number
         y: number
+        elevation: number
+        rotation: number
 
         effects: string[]
         overlayEffect: string
@@ -34,6 +36,8 @@ declare global {
         get height(): BaseTokenFields['height'];
         get x(): BaseTokenFields['x'];
         get y(): BaseTokenFields['y'];
+        get elevation(): BaseTokenFields['elevation'];
+        get rotation(): BaseTokenFields['rotation'];
 
         get effects(): BaseTokenFields['effects'];
         get overlayEffect(): BaseTokenFields['overlayEffect'];
