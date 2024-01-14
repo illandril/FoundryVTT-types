07850003a187364687bd237c61f1379571274851
Joe Spandrusyszyn <git@illandril.net>
2024-01-14 15:44:01 -0500

    Add ui.windows type

--- src/client/variables.d.ts
+++ src/client/variables.d.ts
@@ -2,6 +2,7 @@ declare global {
   interface UI {
     notifications: Notifications
     hotbar: Hotbar
+    windows?: Record<number, Application | undefined>
   }
   const game: Game;
   const ui: UI;
