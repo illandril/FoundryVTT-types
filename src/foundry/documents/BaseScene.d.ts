c27bb9dadcf0792976cb2e42b8c5deb8a7366787
Joe Spandrusyszyn <git@illandril.net>
2024-04-01 22:55:51 -0400

    Add background data to Scene type definitions

--- src/foundry/documents/BaseScene.d.ts
+++ src/foundry/documents/BaseScene.d.ts
@@ -4,12 +4,23 @@ type GridConfiguration = {
   units: string
 };
 
+type TextureData = {
+  src: string | null
+  scaleX: number
+  scaleY: number
+  offsetX: number
+  offsetY: number
+  rotation: number
+  tint: string | null
+};
+
 declare global {
   namespace foundry {
     namespace documents {
       class BaseScene extends foundry.abstract.Document {
         get grid(): GridConfiguration;
         get tokens(): foundry.abstract.EmbeddedCollection<string, BaseToken>;
+        get background(): TextureData;
       }
     }
   }
